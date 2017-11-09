a513d14c59c55ad1638e93c7967867bf6bd01a2d
Philipp Glatza <pglatza@users.noreply.github.com>
2017-11-09 07:01:48 +0100

    typo; 2ndary results to DEVNULL; invocation call debug msg

--- bash/docx_modify.sh
+++ bash/docx_modify.sh
@@ -13,11 +13,11 @@ MODIFY_XPL="$( readlink -f "$3" )"
 XPL="$DIR"/docx_modify/xpl/docx_modify.xpl
 
 if [ -z $XSL ]; then
-    echo "Usage: [DEBUG=yes|no] [HEAP=xxxxm] docx_modify XSL DOCX [XPL]";
+    echo "Usage: [DEBUG=yes|no] [HEAP=xxxxm] [MATHTYPE2OMML=yes|no] docx_modify XSL DOCX [XPL]";
     echo "(The prefixed DEBUG=yes or HEAP=2048m work only if your shell is bash-compatible.)";
     echo "";
     echo "Sample invocation (identity with debug): ";
-    echo "DEBUG=yes ./docx_modifiy.sh lib/xsl/identity.xsl /path/to/myfile.docx";
+    echo "DEBUG=yes ./docx_modify.sh lib/xsl/identity.xsl /path/to/myfile.docx";
     echo "";
     echo "The resulting file will end in .mod.docx, with the same base name.";
     echo "The .docx file's directory will be used to temporarily extract the files.";
@@ -36,8 +36,8 @@ if [ -z $XSL ]; then
     echo "a multi-pass transformation -- if the XPL option had been in place by the time we ";
     echo "wrote this transformation, we would have used it. Now there are two alternative";
     echo "invocations:";
-    echo "./docx_modifiy.sh lib/xsl/page-bookmarks.xsl /path/to/myfile.docx";
-    echo "./docx_modifiy.sh lib/xsl/page-bookmarks.xsl /path/to/myfile.docx lib/xpl/page-bookmarks.xpl";
+    echo "./docx_modify.sh lib/xsl/page-bookmarks.xsl /path/to/myfile.docx";
+    echo "./docx_modify.sh lib/xsl/page-bookmarks.xsl /path/to/myfile.docx lib/xpl/page-bookmarks.xpl";
     echo "";
     echo "If you don't use the mode docx2hub:modify in your custom XSLT, it is important that ";
     echo "you invoke docx2hub:modify's handling of @xml:base attributes. So if one of your modes";
@@ -53,8 +53,12 @@ if [ -z $XSL ]; then
     echo "that is included here but not in a vanilla XML Calabash distribution.";
     echo "";
     echo "Apart from DEBUG, you may prepend LOCALDEFS=/path/to/localdefs.sh for overriding calabash ";
-    echo "settings (see calabash/localdefs.sample.sh). You may also supply the minimum heap space";
-    echo "(java option -Xmx) in a prepended HEAP declaration, e.g., HEAP=2048m";
+    echo "settings. You may also supply the minimum heap space (java option -Xmx) in a prepended ";
+    echo "HEAP declaration, e.g., HEAP=2048m";
+    echo "";
+    echo "The parameter MATHTYPE2OMML (default: no) is for converting MathType formulas into OMML."
+    echo "If you just want you MathType formulas type converted, you may use the following invocation:";
+    echo "MATHTYPE2OMML=yes ./docx_modify.sh lib/xsl/identity.xsl /path/to/myfile.docx";
     exit 1;
 fi
 
@@ -83,10 +87,13 @@ if [ -z $LOCALDEFS ]; then
     LOCALDEFS=$ADAPTIONS_DIR/common/calabash/localdefs.sh
 fi
 
+DEVNULL=/dev/null
+
 if $cygwin; then
   XSL=file:/$(cygpath -ma $XSL)
   DOCX=$(cygpath -ma $DOCX)
   DEBUGDIR=file:/$(cygpath -ma $DEBUGDIR)
+  DEVNULL=$(cygpath -ma /dev/null)
   XPL=file:/$(cygpath -ma $XPL)
   if [ ! -z $MODIFY_XPL ]; then
     MODIFY_XPL=file:/$(cygpath -ma $MODIFY_XPL)
@@ -101,7 +108,11 @@ if [ -z $MATHTYPE2OMML ]; then
   MATHTYPE2OMML=no
 fi
 
-LOCALDEFS=$LOCALDEFS HEAP=$HEAP $DIR/calabash/calabash.sh -D -i xslt="$XSL" $MODIFY_XPL "$XPL" file="$DOCX" mathtype2omml=$MATHTYPE2OMML debug=$DEBUG debug-dir-uri=$DEBUGDIR
+if [ "$DEBUG" == "yes" ]; then
+  echo LOCALDEFS=$LOCALDEFS HEAP=$HEAP $DIR/calabash/calabash.sh -D -i xslt="$XSL" -o result="$DEVNULL" -o modified-single-tree="$DEVNULL" $MODIFY_XPL "$XPL" file="$DOCX" mathtype2omml=$MATHTYPE2OMML debug=$DEBUG debug-dir-uri=$DEBUGDIR
+fi
+
+LOCALDEFS=$LOCALDEFS HEAP=$HEAP $DIR/calabash/calabash.sh -D -i xslt="$XSL" -o result="$DEVNULL" -o modified-single-tree="$DEVNULL" $MODIFY_XPL "$XPL" file="$DOCX" mathtype2omml=$MATHTYPE2OMML debug=$DEBUG debug-dir-uri=$DEBUGDIR
 if [ "$DEBUG" == "no" ]; then
   rm -rf $DOCX.tmp/
 fi
